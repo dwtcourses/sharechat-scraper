5fb4b56c59aacc940608d1963d5ca02648e402b1
Sudeep Duggal <5505558+duggalsu@users.noreply.github.com>
2020-09-10 15:05:38 +0530

    Add handling reposts
    - Added functionality to save reposts as a separate category
    - Added lock files to gitignore

--- sharechat_helper.py
+++ sharechat_helper.py
@@ -221,7 +221,20 @@ def get_post_data(payload_dict, tag_name, tag_translation, tag_genre, bucket_nam
     verified = []
     
     for i in payload_dict.get("payload").get("d"):
-        if i.get("t") == "image":
+        if "repostId" in i:
+            print(i)
+            i["t"] = "repost"
+            if "x" in i.keys():  # if post metadata contains the text
+                get_common_metadata(i, timestamp, language, media_type, post_permalink, caption, external_shares, likes,
+                                    comments, reposts, views, profile_page, verified)
+                if "repostData" in i:
+                    shared_post_url = "https://sharechat.com/post/" + i.get("repostData").get("ph")
+                    text.append(i.get("x") + "<Repost:" + shared_post_url + ">")
+                text.append(i.get("x") + "<Repost>")
+                media_link.append(None)
+            else:
+                pass
+        elif i.get("t") == "image":
             get_common_metadata(i, timestamp, language, media_type, post_permalink, caption, external_shares, likes, comments, reposts, views, profile_page, verified)
             media_link.append(i.get("g"))
             text.append(None)
